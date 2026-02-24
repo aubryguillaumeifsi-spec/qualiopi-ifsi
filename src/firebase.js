a8b6a4dd33c37ec576fc0c4dff3eb22267a1d1e8
aubryguillaumeifsi-spec <aubry.guillaume.ifsi@gmail.com>
2026-02-24 09:39:47 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDY6ZTEOi-g0_98QHHzSJFNgPC0DMTdaus",
+  authDomain: "qualiopi-cham.firebaseapp.com",
+  projectId: "qualiopi-cham",
+  storageBucket: "qualiopi-cham.firebasestorage.app",
+  messagingSenderId: "350382146811",
+  appId: "1:350382146811:web:4962b341a25cd20ebb5c5e"
+};
+
+// Initialisation
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const DOC_REF = doc(db, "qualiopi", "criteres");
