32da209d2e2efbc0c10403aaf4aa75b8a7eca7ea
aubryguillaumeifsi-spec <aubry.guillaume.ifsi@gmail.com>
2026-02-28 12:26:35 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage"; // <-- AJOUT POUR LE STOCKAGE
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDY6ZTEOi-g0_98QHHzSJFNgPC0DMTdaus",
-  authDomain: "qualiopi-cham.firebaseapp.com",
-  projectId: "qualiopi-cham",
-  storageBucket: "qualiopi-cham.firebasestorage.app",
-  messagingSenderId: "350382146811",
-  appId: "1:350382146811:web:4962b341a25cd20ebb5c5e"
+  apiKey: "AIzaSyA_k-WmHFJD7vz3ZGmf-Uin3rue4T-CE6s",
+  authDomain: "qualiopi-tracker-v2.firebaseapp.com",
+  projectId: "qualiopi-tracker-v2",
+  storageBucket: "qualiopi-tracker-v2.firebasestorage.app",
+  messagingSenderId: "561921109114",
+  appId: "1:561921109114:web:0e1e0296b5b918a0ea1fc3"
 };
 
 // Initialisation
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app); // <-- AJOUT POUR LE STOCKAGE
+export const storage = getStorage(app); 
 export const DOC_REF = doc(db, "qualiopi", "criteres");
