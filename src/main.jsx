ac3f9a76fd3585bfc953f062fb969c1b75307e56
aubryguillaumeifsi-spec <aubry.guillaume.ifsi@gmail.com>
2026-03-04 09:46:09 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// --- PATCH TACTILE POUR L'ORGANIGRAMME ---
+import { polyfill } from "mobile-drag-drop";
+import "mobile-drag-drop/default.css"; // Pour l'effet visuel sous le doigt
+
+polyfill({
+  dragImageCenterOnTouch: true
+});
+
+// Force Safari (iPad/iPhone) à autoriser le glissement
+window.addEventListener('touchmove', function() {}, {passive: false});
+// -----------------------------------------
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
